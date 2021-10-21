29f1762475e4ee16f527b8e288e1d2a4c5124f65
thecamboprince <dug21002@byui.edu>
2021-10-20 21:57:02 -0600

    first commit

--- game/board.py
+++ game/board.py
@@ -1,3 +1,38 @@
+import random
+
 #Chase
 class board:
-    pass
+    """
+    """
+
+    def __int__(self):
+        """
+        """
+        self.prepare()
+        self.attempt = []
+        self.guessmove = []
+
+
+    def getguess(self, correct, guess):
+        """
+        """
+        guessoutput = [''] * len(correct)
+
+        for i, (correctnum, guessnum) in enumerate(self.correct, guess):
+            guessoutput[i] = "X"
+            if guessnum == correctnum:
+                print(guessoutput)
+            else:
+                print('0')
+            return ''.join(guessoutput)
+
+
+    def _prepare(self):
+        """ Sets up the board with a random number
+        """
+        attempt = random.randint(0,3)
+        for n in range(attempt):
+            guessmove = random.randint(1, 9)
+            self.attempt.append(guessmove)
+        return attempt
+
--- game/display.py
+++ game/display.py
@@ -3,5 +3,23 @@ class display:
     """
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, prompt):
+        """The class constructor. Gets text input.
+
+		Args:
+			self (Display): an instance of Display
+
+		"""
+        return input(prompt)
+
+
+    def getnum (self, prompt):
+        """
+        """
+        return int(input(prompt))
+
+
+    def gettext (self, text):
+        """
+        """
+        return(text)
