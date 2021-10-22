59204c5a6477b4ef970e0de7e9ce3e04dc4b7a3c
AlexOKeeffe123 <oke20002@byui.edu>
2021-10-22 13:18:17 -0600

    roster and turn

--- game/roster.py
+++ game/roster.py
@@ -1,13 +1,24 @@
 #Alex
 class Roster:
-    def __init__(self, name):
-        pass
+    def __init__(self,):
+        self.player_list = []
 
-    def add_player(self):
-        pass
+    def add_player(self): 
+        num_players= int(input("How many players are playing today? "))
+        for n in range(num_players):
+            name = input(f"Enter the name of player{n+1}: ")
+            self.player_list.append(name)
 
     def get_current(self):
-        pass
+        count=len(self.player_list)
+        self.current_player_index = 0
+        #Starts the list over again
+        if count == self.current_player_index:
+            self.current_player_index = 0
+        else:
+            self.current_player_index +=1
+        
 
     def next_player(self):
-        pass
+        next_play_name = self.player_list[self.current_player_index]
+        print(f"{next_play_name}'s turn: ")
--- game/turn.py
+++ game/turn.py
@@ -2,3 +2,7 @@
 class Turn:
     def __init__(self, guess):
         pass
+
+    def player_guess(self):
+        guess=input("What is your guess? ")
+        return guess
