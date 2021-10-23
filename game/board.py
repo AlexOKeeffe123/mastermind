194974c7162b93cc089ccd0785df4f797a9dd80c
thecamboprince <dug21002@byui.edu>
2021-10-22 22:25:31 -0600

    final commit

--- game/board.py
+++ game/board.py
@@ -4,14 +4,23 @@ from typing import Text
 #Chase
 class Board:
     def __init__(self, length):
-        """
-        """
+        """The class constructor
+
+		Args:
+			self (Display): an instance of Display	
+		"""
         self._items = {} # this is an empty dictionary
         self._solutionLength = length
 
 
     def to_string(self):
-        """
+        """Converts the board data to its string representation.
+
+        Args:
+           self (Board): an instance of Board.
+
+        Returns:
+            string: A representation of the current board.
         """
         lines = "\n--------------"
         for i, (name, values) in enumerate(self._items):
@@ -19,18 +28,27 @@ class Board:
             lines += f"Player {name}: {values[1]}, {values[2]}"
         lines += "\n--------------"
         return lines
-            
+
+
     def apply(self, turn):
+        """ Applies the given turn to the playing surface. Gets player's turn, name, and values
+        
+        Args:
+            self (Board): an instance of Board.
+            turn (Turn): The turn to apply.
+        """
         guesserName = turn.get_guesser_name()
         values = self._items[guesserName]
         values[1] = turn.get_guess()
         values[2] = self._create_hint(values[0], values[1])
+
 
     def prepare(self, player):
             """Sets up the board with an entry for each player.
             
             Args:
                 self (Board): an instance of Board.
+                player (string): gets player's values
             """
             name = player.get_name()
             code = str(random.randint(10 ** (self._solutionLength - 1), 10 ** self._solutionLength))
@@ -64,4 +82,13 @@ class Board:
 
 
     def get_solution(self, name):
+        """ Gets solution
+
+        Args:
+            self (Board): An instance of Board.
+
+        Return:
+            name (String): gets player's name
+            integer (Int): gets code
+        """
         return self._items[name][0]
