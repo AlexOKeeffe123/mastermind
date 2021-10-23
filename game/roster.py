3bb64b1d01398dc1748214cf54447a4e06e15c5d
AlexOKeeffe123 <oke20002@byui.edu>
2021-10-22 18:40:22 -0600

    was looking at the debugging and made changes

--- game/roster.py
+++ game/roster.py
@@ -1,11 +1,15 @@
 #Alex
 class Roster:
-    def __init__(self,name):
+    def __init__(self):
         self.player_list = []
-        self.name = name
+
 
     def add_player(self): 
             self.player_list.append(self.name)
+            player_count= int(input("How many palyers are playing today? "))
+            for p in player_count:
+                name = input(f"What is player{p+1}'s name? ")
+                self.player_list.append(name)
 
     def get_current(self):
         count=len(self.player_list)
